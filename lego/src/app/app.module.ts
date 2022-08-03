2c104f5ee46467da32356410f1b00fba256919f1
caiquecriado18@gmail.com <caiquecriado18@gmail.com>
2022-08-03 14:44:57 -0300

    criacao dos componentes da tela de produtos e ajuste de layouts

--- lego/src/app/app.module.ts
+++ lego/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { LoginComponent } from './login/login.component';
 import { FormularioComponent } from './formulario/formulario.component';
-
+import { ProdutosComponent } from './produtos/produtos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { ProdutosComponent } from './produtos/produtos.component';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     FormularioComponent,
-    ProdutosComponent
+    ProdutosComponent,
+    PerfilComponent,
+    PedidosComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +33,8 @@ import { ProdutosComponent } from './produtos/produtos.component';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
